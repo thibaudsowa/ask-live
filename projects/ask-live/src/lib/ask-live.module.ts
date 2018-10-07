135a051837dd078033ff1625a4fb53d259302ec3
Thibaud Sowa <thibaudsowa@gmail.com>
2018-10-07 23:09:30 +0200

    (feat): add material dependencies and first form

--- projects/ask-live/src/lib/ask-live.module.ts
+++ projects/ask-live/src/lib/ask-live.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { AskLiveComponent } from './ask-live.component';
 import { AdminComponent } from './admin/admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
 
 @NgModule({
   imports: [
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [
     AskLiveComponent,
