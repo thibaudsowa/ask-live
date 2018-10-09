bff253a90a2f4e83e459c348354b80a6fc806d6d
Thibaud Sowa <thibaudsowa@gmail.com>
2018-10-09 08:10:18 +0200

    (feat): connect to firebase and first form

--- projects/ask-live/src/lib/admin/admin.component.ts
+++ projects/ask-live/src/lib/admin/admin.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Question } from '../question';
 
 @Component({
   selector: 'al-admin',
@@ -7,9 +10,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+  questions: Observable<Question[]>;
+  questionForm: Question;
+
+  constructor(private db: AngularFirestore) {
+    this.reset();
+    this.questions = this.db.collection<Question>('questions').valueChanges();
+  }
 
   ngOnInit() {
+  }
+
+  add(): void {
+    this.db.collection<Question>('questions').add(this.questionForm);
+    this.reset();
+  }
+
+  reset(): void {
+    this.questionForm = {
+      subject: '',
+      answer1: '',
+      answer2: '',
+      answer3: '',
+      answer4: ''
+    };
   }
 
 }
--- projects/ask-live/src/lib/ask-live.module.ts
+++ projects/ask-live/src/lib/ask-live.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { AskLiveComponent } from './ask-live.component';
 import { AdminComponent } from './admin/admin.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule } from '@angular/material';
+import { MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule, MatDividerModule, MatListModule } from '@angular/material';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
@@ -10,7 +13,19 @@ import { MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule } fr
     MatFormFieldModule,
     MatInputModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDividerModule,
+    MatListModule,
+    AngularFireModule.initializeApp({
+      apiKey: "AIzaSyCYjTKFajqDRYyqZQeyk97bev5akm6SWbc",
+      authDomain: "presenquestion.firebaseapp.com",
+      databaseURL: "https://presenquestion.firebaseio.com",
+      projectId: "presenquestion",
+      storageBucket: "presenquestion.appspot.com",
+      messagingSenderId: "561863719203"
+    }),
+    AngularFirestoreModule,
+    FormsModule
   ],
   declarations: [
     AskLiveComponent,
