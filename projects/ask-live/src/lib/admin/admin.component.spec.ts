bcb6bce510d4f9183f5826af37ea21c2fdf85ae8
Thibaud Sowa <thibaudsowa@gmail.com>
2018-11-28 08:05:47 +0100

    yip

--- projects/ask-live/src/lib/admin/admin.component.spec.ts
+++ projects/ask-live/src/lib/admin/admin.component.spec.ts
@@ -1,16 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AngularFirestore } from '@angular/fire/firestore';
+import { SharedModule } from '../shared/shared.module';
 import { AdminComponent } from './admin.component';
 
 describe('AdminComponent', () => {
   let component: AdminComponent;
   let fixture: ComponentFixture<AdminComponent>;
 
+  let angularFirestoreStub: Partial<AngularFirestore>;
+
+  angularFirestoreStub = {};
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AdminComponent ]
+      declarations: [AdminComponent],
+      imports: [SharedModule],
+      providers: [{ provide: AngularFirestore, useValue: angularFirestoreStub }]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
--- projects/ask-live/src/lib/shared/shared.module.ts
+++ projects/ask-live/src/lib/shared/shared.module.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule, MatCardModule, MatDialogModule, MatDividerModule, MatExpansionModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule } from '@angular/material';
+import { MatButtonModule, MatCardModule, MatDialogModule, MatDividerModule, MatExpansionModule,
+  MatFormFieldModule, MatIconModule, MatInputModule, MatListModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ConfirmComponent } from './confirm/confirm.component';
 import { ConfirmService } from './confirm/confirm.service';
